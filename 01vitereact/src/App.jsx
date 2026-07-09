79cd7fa07b614d13901a793be6e7596f057b827e
Shubham Raj <sprashar889@gmail.com>
2026-07-09 08:04:30 +0530

    Vite React Basic Concept

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from './chai'
+
+
+function App() {
+ 
+ 
+
+  return (
+    <Chai  />
+              // Technially, in jsx we can return only one element at once,
+              // but to return more than one element, we can use <></> also
+              // known as fragement
+  )
+}
+
+export default App
