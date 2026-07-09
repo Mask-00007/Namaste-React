129020893be1bf7308c6da27ccf6b702abcd7c1f
Shubham Raj <sprashar889@gmail.com>
2026-07-09 07:32:03 +0530

    Hooks Concept Excecuted

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  // This code session is purely based on the hooks concept.
+  // Bascially, let [counter, setCounter] = useState(15);
+  // Here you’re using the useState hook.
+  // useState is a special React function that lets you add state (data that changes over time) to functional components.
+  // It returns an array with two values:
+  // counter → the current state value (initially 15).
+  // setCounter → a function to update that state
+
+  let [counter, setCounter] = useState(15);
+  //let counter = 15;
+
+  const addValue = () => {
+    //counter = counter + 1;
+    setCounter(counter + 1);
+  };
+  const removeValue = () => {
+    //counter = counter + 1;
+    setCounter(counter - 1);
+  };
+  
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter} </h2>
+
+      <button onClick={addValue}>ADD VALUE</button>
+      <br />
+      <button onClick={removeValue}>REMOVE VALUE</button>
+    </>
+  )
+}
+
+export default App
